2697e7edba2add3fe441ca7d22e8a14427ee550e
Webber_Windows <webber26232@gmail.com>
2017-04-02 07:34:46 -0500

    create high dimensional X

--- Feature_selection.py
+++ Feature_selection.py
@@ -2,24 +2,27 @@
 import numpy as np
 
 X = pd.read_csv('X.csv').drop('manager_id',axis=1)
-y = pd.Series(np.ravel(pd.read_csv('y.csv')))
+y = pd.Series(np.ravel(pd.read_csv('y.csv',header=None)))
+print(X.shape)
 
 #from xgboost import XGBClassifier
 #clf = XGBClassifier(objective='softprob',n_estimators=500,max_depth=5,subsample=0.6,colsample_bytree=1,gamma=0.2,min_child_weight=3,reg_lambda=10)
 
 from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier(n_estimators=666,max_features=0.6)
+clf = RandomForestClassifier(n_estimators=666,n_jobs=-1)
 
-
-clf.fit(X,y)
-clf.feature_importance
 '''
+clf.fit(X,y)
+pd.concat([pd.Series(clf.feature_importances_,name = 'importance'),pd.Series(X.columns,name='columne_name')],axis=1).to_csv('RF_feature_importance.csv',index=False)'''
+
+
+
 from sklearn.feature_selection import RFECV
 from sklearn.model_selection import StratifiedKFold
-rfecv = RFECV(clf,cv=StratifiedKFold(10,shuffle=True,random_state=123),scoring='neg_log_loss',step=3,verbose=5)
+rfecv = RFECV(clf,cv=StratifiedKFold(10,shuffle=True,random_state=123),scoring='neg_log_loss',step=50,verbose=10)
 rfecv.fit(X,y)
-pd.DataFrame([X.columns,rfecv.support_,rfecv.ranking_]).to_csv('feature_selection.csv')
-pd.DataFrame([range(1, len(rfecv.grid_scores_) + 1,3),rfecv.grid_scores_]).to_csv('feature_scores.csv')'''
+pd.DataFrame([X.columns,rfecv.support_,rfecv.ranking_]).to_csv('feature_selection_on_manual.csv')
+pd.DataFrame([range(1, len(rfecv.grid_scores_) + 1,3),rfecv.grid_scores_]).to_csv('feature_scores_on_manual.csv')
 
 
 '''
--- create_Xy.py
+++ create_Xy.py
@@ -20,7 +20,7 @@ adrs['building'] = adrs['building'].where(adrs['building']!=' ',adrs['formatted_
 
 median = df[['latitude','longitude']].median()
 mahathen_dist = (df[['latitude','longitude']] - median).abs().sum(axis=1)
-df['bad_codint_quality'] = (mahathen_dist>mahathen_dist.quantile(0.995))*1
+#df['bad_codint_quality'] = (mahathen_dist>mahathen_dist.quantile(0.995))*1
 new_lat = df.latitude.where(mahathen_dist<=mahathen_dist.quantile(0.995),original_addresses.map(adrs.set_index('original_adrs')['lat']))
 new_lng = df.longitude.where(mahathen_dist<=mahathen_dist.quantile(0.995),original_addresses.map(adrs.set_index('original_adrs')['lng']))
 df.latitude = df.latitude.where(new_lat.isnull(),new_lat)
@@ -42,21 +42,22 @@ df['street_code'] = encode_by_count(street_name)
 del street_name, adrs, original_addresses, median, mahathen_dist
 
 #df[['listing_id','formatted_adrs','latitude','longitude']].to_csv('cood.csv',index=False)
-'''
+
 ifm_dict = {'convenience_store':[500],
 'home_goods_store':[500],
 'department_store':[2000],
 'bar':[500],'cafe':[500],'restaurant':[300],
 'train_station':[3000],'bus_station':[1000],#[1000,300],
 'subway_station':[2000],#[2000,500],
-'laundry':[1000],'bank':[1000],'pharmacy':[1000],'church':[1000],'school':[500]}'''
+'laundry':[1000],'bank':[1000],'pharmacy':[1000],'church':[1000],'school':[500]}
+'''
 ifm_dict = {'convenience_store':[500],
 'restaurant':[300],
 'train_station':[3000],
 'subway_station':[2000],
 'pharmacy':[1000],
 'church':[1000],
-'school':[500]}
+'school':[500]}'''
 
 ambs_features = [ifm+'_'+str(x) for ifm in ifm_dict for x in ifm_dict[ifm]]
 ambs = pd.read_csv('cood_with_ambs_patch.csv',encoding='gbk')
@@ -88,27 +89,30 @@ def create_area_code(frame,cut_size,target_column_prefix,method='width',keep='co
     else:
         raise ValueError('keep should be code or count or both')
 	
-'''df = create_area_code(df,400,'area_large')
+df = create_area_code(df,400,'area_large')
 df = create_area_code(df,1200,'area_medium')
 df = create_area_code(df,2000,'area_small')
 df = create_area_code(df,20,'area_qlarge','depth')
 df = create_area_code(df,40,'area_qmedium','depth')
-df = create_area_code(df,60,'area_qsmall','depth')'''
-df = create_area_code(df,50,'area_qsmall','depth')
+df = create_area_code(df,60,'area_qsmall','depth')
+#df = create_area_code(df,50,'area_qsmall','depth')
 
 df['building_code'] = encode_by_count(df.building_id)
 df['adrs_code'] = encode_by_count(df.formatted_adrs)
 
-adrs_features = ['bad_codint_quality','adrs_quality']
+#adrs_features = ['bad_codint_quality','adrs_quality']
+adrs_features = ['adrs_quality']
 
 df['num_photos'] = df['photos'].apply(len)
 df['num_features'] = df['features'].apply(len)
 df['num_description_words'] = df['description'].apply(lambda x: len(x.split(' ')))
+
 df['created'] = pd.to_datetime(df['created'])
 df['created_month'] = df['created'].dt.month
 df['created_day'] = df['created'].dt.day
 df['created_day_of_year'] = df['created'].dt.dayofyear
 df['created_day_of_week'] = df['created'].dt.dayofweek
+df['created_tens_of_month'] = df['created_day'].apply(lambda x:int(str(x).zfill(2)[0])).replace(3,2)
 
 df.bedrooms = df.bedrooms.replace(0,0.5)
 df.bathrooms = df.bathrooms.replace(0,0.5)
@@ -138,11 +142,9 @@ df['dist_to_kt'] = df[['latitude','longitude']].apply(lambda x:distance.vincenty
 distance_features = ['dist_to_tmsq','dist_to_ctpk','dist_to_wtc','dist_to_bkc','dist_to_ap','dist_to_kt']
 
 
-
-
 features_to_use = ['bathrooms', 'bedrooms', 'rooms', 'latitude', 'longitude', 'price',
                    'num_photos', 'num_features', 'num_description_words',
-                   'created_day_of_year', 'created_month', 'created_day_of_week', 'created_day',
+                   'created_day_of_year', 'created_month', 'created_day_of_week', 'created_day','created_tens_of_month',
                    'bed_bath_rate','bed_bath_diff',
                    'price_per_bath','price_per_bed','price_per_room']
 
@@ -150,30 +152,25 @@ features_to_use.remove('created_month')
 features_to_use.remove('bed_bath_diff')
 features_to_use.remove('bed_bath_rate')
 
-'''category_features = ['building_code','street_code','adrs_code',
+category_features = ['building_code','street_code','adrs_code',
                      'area_large_code','area_medium_code','area_small_code',
-                     'area_qlarge_code','area_qmedium_code','area_qsmall_code',
-                     ]
+                     'area_qlarge_code','area_qmedium_code','area_qsmall_code']
+
 metrics = ['price','price_per_bed','price_per_bath','price_per_room',
            'bed_bath_rate','bed_bath_diff',
            'num_features','num_photos','num_description_words',
            'created_month','created_day','created_day_of_week','created_day_of_year']
-'''
 
-category_features = ['building_code','street_code','adrs_code','area_qsmall_code',
-                     'created_month','created_day','created_day_of_week',
-                     'bedrooms','bathrooms','rooms']
-category_features.remove('created_month')
 					 
 metrics = ['price','price_per_bed','price_per_bath','price_per_room',
            'bed_bath_rate','bed_bath_diff',
            'num_features','num_photos','num_description_words',
-           'created_day_of_year'] + ambs_features + distance_features
+           'created_day_of_year'] + ambs_features + distance_features + adrs_features
 		   
 metrics.remove('bed_bath_diff')
 
 cat_features = []
-for cat in category_features:
+for cat in category_features + ['created_month','created_day','created_day_of_week', 'created_tens_of_month','bedrooms','bathrooms','rooms']:
     for met in metrics:
         if (met in ambs_features  or met in distance_features) and 'code' in cat:
             break
@@ -188,24 +185,26 @@ for cat in category_features:
         df['diff_from_'+cat+'_median_'+met] = df[met] - df[cat+'_median_'+met]
         df['rank_of_'+cat+'_'+met] = gb_object.rank(method='average',pct=True,ascending=False)
 		
-cat_features = [x for x in cat_features if not x.startswith('diff')] + [x for x in cat_features if x.startswith('diff') and not x.endswith('bed_bath_rate')]
+#cat_features = [x for x in cat_features if not x.startswith('diff')] + [x for x in cat_features if x.startswith('diff') and not x.endswith('bed_bath_rate')]
 
 
 num_cut_features = []
 
 for met_to_cut in metrics:
+    if met_to_cut in ambs_features:
+        continue
     #large_group = pd.cut(df[met_to_cut],bins=5)
     #small_group = pd.cut(df[met_to_cut],bins=10)
-    print(met_to_cut,df[met_to_cut].unique().size)
-    qlarge_group = pd.qcut(df[met_to_cut],[float(x)/5 for x in range(6)])
+    if met_to_cut in distance_features:
+        qlarge_group = pd.qcut(df[met_to_cut],[float(x)/10 for x in range(11)])
+    else:
+        qlarge_group = pd.qcut(df[met_to_cut],[float(x)/5 for x in range(6)])
     #qsmall_group = pd.qcut(df[met_to_cut],[float(x)/10 for x in range(11)])
     for met_to_cal in metrics:
         if met_to_cut == met_to_cal:
-            break
-        if met_to_cut in ambs_features and met_to_cal in ambs_features:
-            break
-		if met_to_cut in distance_features and met_to_cal in distance_features:
-            break
+            continue
+        if met_to_cut in distance_features and met_to_cal in distance_features:
+            continue
         num_cut_features.extend([met_to_cut+'_group_median_'+met_to_cal,met_to_cut+'_group_std_'+met_to_cal,'diff_from_'+met_to_cut+'_group_median_'+met_to_cal,'rank_of_'+met_to_cut+'_group_'+met_to_cal])
         gb_object = df.groupby(qlarge_group)[met_to_cal]
         df[met_to_cut+'_group_median_'+met_to_cal] = gb_object.transform(lambda x:x.median())
@@ -219,7 +218,7 @@ for met_to_cut in metrics:
 features = list(set(features_to_use + cat_features + num_cut_features + category_features + adrs_features + ambs_features + distance_features))
 X = df[features+['manager_id']]
 
-y = df[df.interest_level.notnull()]['interest_level'].map({'high':0,'medium':1,'low':2})
+#y = df[df.interest_level.notnull()]['interest_level'].map({'high':0,'medium':1,'low':2})
 
 X.to_csv('X.csv',index=False)
 #y.to_csv('y.csv',index=False)
