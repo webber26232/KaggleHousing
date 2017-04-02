7252868df0f7a7ad4cec2638f93ac0aff83ddbeb
Webber <webber26232@gmail.com>
2017-04-02 07:47:14 -0500

    correct feature output

--- Feature_selection.py
+++ Feature_selection.py
@@ -8,8 +8,8 @@ print(X.shape)
 #from xgboost import XGBClassifier
 #clf = XGBClassifier(objective='softprob',n_estimators=500,max_depth=5,subsample=0.6,colsample_bytree=1,gamma=0.2,min_child_weight=3,reg_lambda=10)
 
-from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier(n_estimators=666,n_jobs=-1)
+from sklearn.ensemble import ExtraTreeClassifier
+clf = ExtraTreeClassifier(n_estimators=666,n_jobs=-1)
 
 '''
 clf.fit(X,y)
@@ -19,10 +19,10 @@ pd.concat([pd.Series(clf.feature_importances_,name = 'importance'),pd.Series(X.c
 
 from sklearn.feature_selection import RFECV
 from sklearn.model_selection import StratifiedKFold
-rfecv = RFECV(clf,cv=StratifiedKFold(10,shuffle=True,random_state=123),scoring='neg_log_loss',step=50,verbose=10)
+rfecv = RFECV(clf,cv=StratifiedKFold(10,shuffle=True,random_state=123),scoring='neg_log_loss',step=20,verbose=10)
 rfecv.fit(X,y)
-pd.DataFrame([X.columns,rfecv.support_,rfecv.ranking_]).to_csv('feature_selection_on_manual.csv')
-pd.DataFrame([range(1, len(rfecv.grid_scores_) + 1,3),rfecv.grid_scores_]).to_csv('feature_scores_on_manual.csv')
+pd.DataFrame([X.columns,rfecv.support_,rfecv.ranking_]).T.to_csv('feature_selection_on_manual.csv')
+pd.DataFrame([range(1, len(rfecv.grid_scores_) + 1),rfecv.grid_scores_]).T.to_csv('feature_scores.csv')
 
 
 '''
